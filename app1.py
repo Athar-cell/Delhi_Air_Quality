7e1acd5b56cccafd44e27860fad848317425ae4a
Athar Sharma <atharsharma86@gmail.com>
2025-08-30 15:20:39 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,14 +13,15 @@ if st.checkbox("Show Raw Data"):
     st.write(df.head())
 
 # Filter by year/month
-year = st.selectbox("Select Year", df['year'].unique())
-filtered_df = df[df['year'] == year]
+Year = st.selectbox("Select Year", df['Year'].unique())
+filtered_df = df[df['Year'] == Year]
 
-st.write(f"Air Quality Data for {year}")
+st.write(f"Air Quality Data for {Year}")
 st.line_chart(filtered_df['PM2.5'])
 
 # Custom Matplotlib plot
 fig, ax = plt.subplots()
 filtered_df['PM2.5'].plot(kind='hist', bins=30, ax=ax)
 st.pyplot(fig)
+
 
